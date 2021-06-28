bb83ef593585fa8f455664d979a7f7d6cba7d637
jsphuebner <dev@johanneshuebner.com>
2021-06-28 22:26:18 +0200

    Added crude lockout flag to inhibit sending mapped messages while map is invalid

--- include/param_prj.h
+++ include/param_prj.h
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define VER 1.15.R
+#define VER 1.16.R
 
 
 /* Entries must be ordered as follows:
