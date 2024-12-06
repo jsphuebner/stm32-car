9435b7362508b5a441ca8d620704234787170051
jsphuebner <dev@johanneshuebner.com>
2024-12-06 09:14:04 +0100

    Corrected duplicate ID

--- include/param_prj.h
+++ include/param_prj.h
@@ -114,12 +114,12 @@
     VALUE_ENTRY(din_bms,      ONOFF,   2032 ) \
     VALUE_ENTRY(din_charge,   ONOFF,   2084 ) \
     VALUE_ENTRY(din_bmslock,  ONOFF,   2054 ) \
-    VALUE_ENTRY(dout_dcsw,    ONOFF,   2078 ) \
+    VALUE_ENTRY(dout_dcsw,    ONOFF,   2086 ) \
     VALUE_ENTRY(handbrk,      ONOFF,   2071 ) \
     VALUE_ENTRY(espoff,       ONOFF,   2077 ) \
     VALUE_ENTRY(cpuload,      "%",     2035 ) \
 
-//Next value Id: 2086
+//Next value Id: 2087
 
 #define VERSTR STRINGIFY(4=VER)
 #define OPMODES      "0=Off, 1=Run, 2=ChargeStart, 3=ConnectorLock, 4=Charge, 5=ChargeStop"
